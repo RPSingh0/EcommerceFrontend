3381f8acc96d27abf4594f494af8f26ac65ca0be
RPSingh0 <rpalsingh715@gmail.com>
2024-08-25 08:10:51 +0530

    Added support for github pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import {HashRouter, Navigate, Route, Routes} from "react-router-dom";
 import AppLayout from "./components/AppLayout/AppLayout.jsx";
 import BestOfSubCategory from "./Features/Dashboard/BestOfSubCategory.jsx";
 import PageNotFound from "./Features/404NotFound/PageNotFound.jsx";
@@ -31,7 +31,7 @@ export default function App() {
     return (
         <QueryClientProvider client={queryClient}>
             <ReactQueryDevtools initialIsOpen={false}/>
-            <BrowserRouter>
+            <HashRouter>
                 <Routes>
                     <Route element={<AppLayout/>}>
                         <Route index element={<Navigate replace={true} to={"home"}/>}/>
@@ -75,7 +75,7 @@ export default function App() {
                         <Route path={"*"} element={<PageNotFound/>}/>
                     </Route>
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
             <Toaster position={"botton-center"} gutter={12} containerStyle={{margin: '8px'}} toastOptions={{
                 success: {
                     duration: 3000,
